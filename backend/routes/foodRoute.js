44dc1c92785ef1a24a951b3338e74e239f56fa81
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-01-23 13:29:46 +0530

    add, remove and get food list logic controller done.

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addfood } from '../controllers/foodController.js'
+import { addfood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer'
 
 const foodRouter = express.Router();
@@ -17,6 +17,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage})
 
 foodRouter.post("/add",upload.single("image"), addfood);
-
+foodRouter.get("/list", listFood);
+foodRouter.post('/remove', removeFood);
 
 export default foodRouter;

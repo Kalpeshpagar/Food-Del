f8d5b87f3c45a8692f291b9f0c57b0a422136164
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-01-25 12:33:38 +0530

    List item component done

--- admin/pages/Add/Add.jsx
+++ admin/pages/Add/Add.jsx
@@ -3,8 +3,7 @@ import './Add.css'
 import { assets } from '../../src/assets/assets'
 import axios from 'axios'
 
-const Add = () => {
-    const url = 'http://localhost:4000';
+const Add = ({url}) => {
     const [image, setImage] = useState(false);
     const [data,setData] = useState({
         name:"",

85c2f0fc90cc18c149aad7536d0f0f517b6e4b78
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-01-28 12:31:38 +0530

    feat: Implement order placement, verification, and user order retrieval functionalities with authentication middleware.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,22 @@
 import jwt from 'jsonwebtoken'
 
-const authMiddleware = async(req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const token = req.headers.token;
- 
+
     if (!token) {
-        return res.json({success:false,message:"Not Authorized Login agian"})
+        return res.json({ success: false, message: "Not Authorized Login agian" })
     }
     try {
-        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+        if (!req.body) {
+            req.body = {};
+        }
         req.body.userId = token_decode.id
         next()
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
-        
+        res.json({ success: false, message: "Error" })
+
     }
 
 }

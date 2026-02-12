5c4abc2e12682a637fcbce25219226853dcf673c
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2026-02-12 20:13:03 +0530

    Update database connection to use environment variable for URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kalpeshpagar57:7666286312@cluster0.cwer1yj.mongodb.net/food-del').then(()=>console.log("DB connected"))
+    await mongoose.connect(`${process.env.DATABASE_URL}/food-del`).then(()=>console.log("DB connected"))
 }
